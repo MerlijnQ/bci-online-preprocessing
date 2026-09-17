54ab101d08c31babf775a083fa55bdbdbcfd285d
Gianluca <g.fuschi@student.rug.nl>
2026-09-17 18:36:13 +0200

    cleaned and finished the code for online vs offline comparison

--- examples/offline_vs_online.py
+++ examples/offline_vs_online.py
@@ -36,34 +36,16 @@ phase_difference_deg = np.degrees(phase_difference_rad)
 
 # phase_difference_deg = np.mean(phase_offline - phase_online)
 
-print(phase_difference_deg)
-
-# https://medium.com/data-science/instantaneous-phase-and-magnitude-with-the-hilbert-transform-40a73985be07
-# https://medium.com/@RaghavKrishna25/hilbert-transform-in-action-from-signals-to-insights-206edb017f2f
-# https://www.youtube.com/watch?v=7CimsUF8jwI
-phase_offline = np.angle(hilbert(offline)) 
-phase_online = np.angle(hilbert(online))
+print(f"the phase shift is {phase_difference_deg} degrees")
 
 
-phase_difference_rad = np.angle(
-    np.mean(
-        np.exp(1j * (phase_offline - phase_online))
-    )
-)
-
-phase_difference_deg = np.degrees(phase_difference_rad)
-
-# phase_difference_deg = np.mean(phase_offline - phase_online)
-
-print(phase_difference_deg)
-
+# https://stackoverflow.com/questions/6157791/find-phase-difference-between-two-inharmonic-waves
+# https://dsp.stackexchange.com/questions/59036/cross-correlation-dimensions
+# https://www.youtube.com/watch?v=0yFdKsv5L-U
 
 # returns an array of the discrete cross-correlation between the two filtered signals
 # the length of the cross-correlation is 2*N - 1 because it looks at the shift in both direction
-# so it's double the number of samples
 xcorr = correlate(online, offline)
-print(f"the length of the cross-correlation is {len(xcorr)}")
-print(xcorr.argmax())
 
 
 # n is the number of samples (we could also use FS)
@@ -71,16 +53,17 @@ n = len(offline)
 # delta time array to match cross-correlation
 dt = np.arange(1-n, n)
 
+
+# get the index of the 
 recovered_time_shift = dt[xcorr.argmax()]
+# time shift in milliseconds
+time_shift_seconds = recovered_time_shift * 1/250 * 1000
 
+print(f"the time delay is {time_shift_seconds} milliseconds or {time_shift_seconds / 1000} seconds")
 
-
-print(f"phase shift leads to a delay at the {recovered_time_shift}th sample")
-print(f"the time delay is {recovered_time_shift * 1/250}")
-
-# plt.plot(offline, label="offline (filtfilt)")
-# plt.plot(online, label="online (causal)")
-# plt.legend()
-# plt.title("Offline vs Online Filtering")
-# plt.show()
+plt.plot(offline, label="offline (filtfilt)")
+plt.plot(online, label="online (causal)")
+plt.legend()
+plt.title("Offline vs Online Filtering")
+plt.show()
 
