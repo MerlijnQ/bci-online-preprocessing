ef910207ee1d95389cdc5f8884449c3b83d216a4
MerlijnQ <m.q.mulder@student.rug.nl>
2026-09-24 14:37:00 +0200

    Changed the windowing script such that if follows  the initial order of code provided

--- src/windowing.py
+++ src/windowing.py
@@ -10,37 +10,60 @@ class SlidingWindow:
         self.step = step
         self.channels = channels
         self.buffer = np.empty((0, channels))
-        self.step_counter = 0
+        self.counter = 0
         self.is_full = False
 
-    def update(self, data):
-        # Accepts a single sample of shape (channels,) 
-        # or a chunk of shape (n_samples, channels).
-        data = np.asarray(data)
+    def update(self, sample):
 
-        self.buffer = np.vstack((self.buffer, data))
+        sample = np.array(sample)
+        self.buffer = np.vstack((self.buffer, sample))
 
-        # Check
-        if not self.is_full:
-            # check it window is full
-            if len(self.buffer) >= self.size:
-                self.is_full = True
-                # when window is full, restart counter
-                self.step_counter = 0
-                # Return most recent window 
-                return self.buffer[-self.size:].T
-            return None
+        if self.is_full:
+            self.counter += len(sample)
 
-        # increase the counter. This also works for chunks of samples
-        self.step_counter += len(data)
+        if len(self.buffer) > self.size:
+            self.buffer = self.buffer[-self.size:]      
 
-        # if there are too many samples
-        if self.step_counter >= self.step:
-            self.step_counter = 0
-            # Trim excess historical buffer to prevent unbounded growth
-            if len(self.buffer) > self.size + self.step:
-                self.buffer = self.buffer[-self.size:]
-            return self.buffer[-self.size:].T
+        if len(self.buffer) == self.size and self.counter % self.step == 0:
+       
+            self.is_full = True
+            
+            if len(self.buffer.shape) > 1:
+                return self.buffer.T
+            else:
+                return self.buffer
+            
 
         return None
+
+    # def update(self, data):
+    #     # Accepts a single sample of shape (channels,) 
+    #     # or a chunk of shape (n_samples, channels).
+    #     data = np.asarray(data)
+
+    #     self.buffer = np.vstack((self.buffer, data))
+
+    #     # Check
+    #     if not self.is_full:
+    #         # check it window is full
+    #         if len(self.buffer) >= self.size:
+    #             self.is_full = True
+    #             # when window is full, restart counter
+    #             self.step_counter = 0
+    #             # Return most recent window 
+    #             return self.buffer[-self.size:].T
+    #         return None
+
+    #     # increase the counter. This also works for chunks of samples
+    #     self.step_counter += len(data)
+
+    #     # if there are too many samples
+    #     if self.step_counter >= self.step:
+    #         self.step_counter = 0
+    #         # Trim excess historical buffer to prevent unbounded growth
+    #         if len(self.buffer) > self.size + self.step:
+    #             self.buffer = self.buffer[-self.size:]
+    #         return self.buffer[-self.size:].T
+
+    #     return None
 
